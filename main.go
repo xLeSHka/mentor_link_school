bff42cf8fdc6895c638f96ed2e94824112e62225
xLeSHka <148469104+xLeSHka@users.noreply.github.com>
2025-02-28 21:01:37 +0300

    docker compose

--- main.go
+++ main.go
@@ -9,9 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
-	go func() {
 
-		log.Fatal(http.ListenAndServe("prod-team-14-mkg8u20m.final.prodcontest.ru:80", nil))
-	}()
-	log.Println("Starting server...")
+	log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
+
 }
