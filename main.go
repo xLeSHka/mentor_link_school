f523433c97c082bc904f9822837ace3663b0d325
xLeSHka <148469104+xLeSHka@users.noreply.github.com>
2025-02-28 20:05:15 +0300

    ssl

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
+	go func() {
 
-	log.Fatal(http.ListenAndServe("prod-team-14-mkg8u20m.final.prodcontest.ru:80", nil))
+		log.Fatal(http.ListenAndServe("prod-team-14-mkg8u20m.final.prodcontest.ru:80", nil))
+	}()
+	log.Println("Starting server...")
 }
