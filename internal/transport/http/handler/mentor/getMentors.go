78e6cc1c9261c72eb33e5121f4cf35795b6e998e
ainur <ainur.salahiev21@mail.ru>
2025-03-01 10:19:07 +0300

    fix

--- internal/transport/http/handler/mentor/getMentors.go
+++ internal/transport/http/handler/mentor/getMentors.go
@@ -1,24 +1,20 @@
 package mentorsRoute
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"gitlab.prodcontest.ru/team-14/lotti/internal/app/httpError"
-	"gitlab.prodcontest.ru/team-14/lotti/internal/models"
-	usersRoute "gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/handler/user"
-	"net/http"
 )
 
 func (h *Route) getMentors(c *gin.Context) {
 	personId := uuid.MustParse(c.MustGet("personId").(string))
 
-
-
-	mentors,err := h.mentorService.GetMentors(c.Request.Context(), personId)
+	mentors, err := h.mentorService.GetMentors(c.Request.Context(), personId)
 	if err != nil {
 		err.(*httpError.HTTPError).SendError(c)
 		return
 	}
 	c.Writer.WriteHeader(http.StatusCreated)
 }
-
