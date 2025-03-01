9ed8be1463aba8d4cb1cacda9a00ebcaa0de4452
mimimix <lmxsp@mail.ru>
2025-03-02 00:50:14 +0300

    fix models

--- internal/models/models.go
+++ internal/models/models.go
@@ -14,7 +14,7 @@ type User struct {
 	AvatarURL *string
 	BIO       *string
 	Telegram  string `gorm:"not null"`
-	Role      *Role  `gorm:"foreignKey:id"`
+	Role      *Role  `gorm:"foreignKey:user_id"`
 }
 
 func (_ *User) TableName() string {
@@ -71,8 +71,8 @@ type Pair struct {
 	MentorID uuid.UUID `gorm:"type:uuid;not null"`
 	GroupID  uuid.UUID `gorm:"type:uuid;not null"`
 	Goal     string    `gorm:"not null"`
-	Mentor   *User     `gorm:"foreignKey:id"`
-	Student  *User     `gorm:"foreignKey:id"`
+	Mentor   *User     `gorm:"foreignKey:mentor_id"`
+	Student  *User     `gorm:"foreignKey:user_id"`
 }
 
 func (_ *Pair) TableName() string {
