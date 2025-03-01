85cf6a17ae10247efde7614af28c5a46bf8585df
ainur <ainur.salahiev21@mail.ru>
2025-03-01 23:24:02 +0300

    fix login

--- internal/repository/user/login.go
+++ internal/repository/user/login.go
@@ -3,6 +3,7 @@ package repositoryUser
 import (
 	"context"
 	"errors"
+
 	"gitlab.prodcontest.ru/team-14/lotti/internal/models"
 	"gorm.io/gorm"
 )
@@ -11,12 +12,12 @@ func (r *UsersRepository) Login(ctx context.Context, person *models.User) (*mode
 	//err := r.DB.WithContext(ctx).FirstOrCreate(person).Error
 	err := r.DB.WithContext(ctx).First(&person, &models.User{Name: person.Name}).Error
 	if err != nil {
-		if !errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			if err := r.DB.WithContext(ctx).Create(person).Error; err != nil {
+				return nil, err
+			}
+			return person, nil
 		}
-	}
-	err = r.DB.WithContext(ctx).Create(person).Error
-	if err != nil {
 		return nil, err
 	}
 	return person, nil
