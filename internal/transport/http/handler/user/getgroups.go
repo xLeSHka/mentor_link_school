6e85255d657c669949333e6c1fd11ace234b3162
mimimix <lmxsp@mail.ru>
2025-03-03 01:53:25 +0300

    new fields

--- internal/transport/http/handler/user/getgroups.go
+++ internal/transport/http/handler/user/getgroups.go
@@ -1,50 +1,42 @@
 package usersRoute
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"gitlab.prodcontest.ru/team-14/lotti/internal/app/httpError"
-	"gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/pkg/jwt"
-)
-
 // @Summary Получить список моих запросов
 // @Schemes
 // @Tags Users
 // @Accept json
 // @Produce json
-// @Router /api/user/requests [get]
+// @Router !!!!!!/api/user/requests [get]
 // @Success 200 {object} []respGetHelp
-func (h *Route) getGroups(c *gin.Context) {
-	personId, err := jwt.Parse(c)
-	if err != nil {
-		err.(*httpError.HTTPError).SendError(c)
-		c.Abort()
-		return
-	}
-	var req reqGetRole
-	if err := h.validator.ShouldBindQuery(c, &req); err != nil {
-		httpError.New(http.StatusBadRequest, err.Error()).SendError(c)
-		c.Abort()
-		return
-	}
-	groups, err := h.usersService.GetGroups(c.Request.Context(), personId)
-	if err != nil {
-		err.(*httpError.HTTPError).SendError(c)
-		return
-	}
-	resp := make([]*respGetGroupDto, 0, len(groups))
-	for _, g := range groups {
-		if g.Group.AvatarURL != nil {
-			avatarURL, err := h.minioRepository.GetImage(*g.Group.AvatarURL)
-			if err != nil {
-				httpError.New(http.StatusInternalServerError, err.Error()).SendError(c)
-				c.Abort()
-				return
-			}
-			g.Group.AvatarURL = &avatarURL
-		}
-		resp = append(resp, mapGroup(g, req.Role))
-	}
-	c.JSON(http.StatusOK, resp)
-}
+//func (h *Route) getGroups(c *gin.Context) {
+//	personId, err := jwt.Parse(c)
+//	if err != nil {
+//		err.(*httpError.HTTPError).SendError(c)
+//		c.Abort()
+//		return
+//	}
+//	var req reqGetRole
+//	if err := h.validator.ShouldBindQuery(c, &req); err != nil {
+//		httpError.New(http.StatusBadRequest, err.Error()).SendError(c)
+//		c.Abort()
+//		return
+//	}
+//	groups, err := h.usersService.GetGroups(c.Request.Context(), personId)
+//	if err != nil {
+//		err.(*httpError.HTTPError).SendError(c)
+//		return
+//	}
+//	resp := make([]*respGetGroupDto, 0, len(groups))
+//	for _, g := range groups {
+//		if g.Group.AvatarURL != nil {
+//			avatarURL, err := h.minioRepository.GetImage(*g.Group.AvatarURL)
+//			if err != nil {
+//				httpError.New(http.StatusInternalServerError, err.Error()).SendError(c)
+//				c.Abort()
+//				return
+//			}
+//			g.Group.AvatarURL = &avatarURL
+//		}
+//		resp = append(resp, mapGroup(g, req.Role))
+//	}
+//	c.JSON(http.StatusOK, resp)
+//}
