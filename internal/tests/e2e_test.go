0e3f38a2944c1ecc47a7f739c3225d4c7d4e504f
xLeSHka <148469104+xLeSHka@users.noreply.github.com>
2025-03-02 03:36:19 +0300

    tests

--- internal/tests/e2e_test.go
+++ internal/tests/e2e_test.go
@@ -1,8 +1,13 @@
 package tests
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
+	jwtlib "github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"gitlab.prodcontest.ru/team-14/lotti/internal/models"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -17,8 +22,6 @@ import (
 	"github.com/minio/minio-go/v7"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"gitlab.prodcontest.ru/team-14/lotti/internal/app"
 	"gitlab.prodcontest.ru/team-14/lotti/internal/app/Validators"
 	db2 "gitlab.prodcontest.ru/team-14/lotti/internal/connetions/db"
 	minio2 "gitlab.prodcontest.ru/team-14/lotti/internal/connetions/minio"
@@ -33,14 +36,9 @@ import (
 	publicRoute "gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/handler/public"
 	usersRoute "gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/handler/user"
 	jwt2 "gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/pkg/jwt"
-	"go.uber.org/fx"
+
 	"gorm.io/gorm"
 )
-
-func TestValidateApp(t *testing.T) {
-	err := fx.ValidateApp(app.App)
-	require.NoError(t, err)
-}
 
 var quit = make(chan os.Signal, 1)
 var config config2.Config
@@ -55,6 +53,8 @@ var validator *Validators.Validator
 var UserService service.UserService
 
 var routers *ApiRouters.ApiRouters
+var profile1JWT string
+var unknownJWT string
 
 func init() {
 	gin.SetMode(gin.TestMode)
@@ -63,6 +63,7 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	db, err = db2.New(config)
 	if err != nil {
 		log.Fatal(err)
@@ -93,16 +94,28 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	profile1JWT, err = jwt.CreateToken(jwtlib.MapClaims{
+		"id": profile1.ID,
+	}, time.Now().Add(time.Hour*6))
+	if err != nil {
+		log.Fatal(err)
+	}
+	unknownJWT, err = jwt.CreateToken(jwtlib.MapClaims{
+		"id": uuid.New(),
+	}, time.Now().Add(time.Hour*6))
+	if err != nil {
+		log.Fatal(err)
+	}
 	UserRepository = repositoryUser.New(db)
 	MinioRepository = repository2.New(minioClient, config)
 
 	UserService = userService.New(userService.FxOpts{
 		JWT:             jwt,
-		RDB:             rdb,
 		UsersRepository: UserRepository,
 		MinioRepository: MinioRepository,
+		Config:          config,
 	})
-	routers = ApiRouters.CreateApiRoutes(http3, db, rdb, jwt)
+	routers = ApiRouters.CreateApiRoutes(http3, jwt)
 
 	publicRoute.PublicRoute(routers)
 	usersRoute.UsersRoute(usersRoute.FxOpts{
@@ -118,6 +131,10 @@ func setUp() (func(), chan os.Signal, error) {
 		return nil, nil, err
 	}
 	db.Exec("DELETE FROM public.users;")
+	db.Exec("DELETE FROM public.groups;")
+	db.Exec("DELETE FROM public.pairs;")
+	db.Exec("DELETE FROM public.help_requests;")
+	db.Exec("DELETE FROM public.roles;")
 	srv := &http.Server{
 		Addr:    config.ServerAddress,
 		Handler: http3,
@@ -145,7 +162,12 @@ func setUp() (func(), chan os.Signal, error) {
 	log.Println("server started")
 	return fn, quit, nil
 }
-func TestTemplate(t *testing.T) {
+
+type Name struct {
+	Name string `json:"name"`
+}
+
+func TestLogin(t *testing.T) {
 	fn, quit, err := setUp()
 	assert.Nil(t, err)
 	defer func() {
@@ -153,19 +175,28 @@ func TestTemplate(t *testing.T) {
 		fn()
 	}()
 	type Test struct {
-		//
+		Name         Name
 		name         string
 		expectedCode int
 	}
 	tests := []Test{
-		{},
+		{
+			Name:         Name{"User1"},
+			name:         "create user",
+			expectedCode: http.StatusOK,
+		},
+		{
+			Name:         Name{"User1"},
+			name:         "login user",
+			expectedCode: http.StatusOK,
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			//jsonData, _ := json.Marshal()
-			url := "/user"
-			req, _ := http.NewRequest(http.MethodPost, url, nil) // bytes.NewBuffer(jsonData)
-			//req.Header.Set("Content-Type", "application/json")
+			jsonData, _ := json.Marshal(test.Name)
+			url := "/api/user/auth/sign-in"
+			req, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData)) // bytes.NewBuffer(jsonData)
+			req.Header.Set("Content-Type", "application/json")
 
 			w := httptest.NewRecorder()
 			http3.ServeHTTP(w, req)
@@ -174,7 +205,76 @@ func TestTemplate(t *testing.T) {
 				assert.Nil(t, err)
 			}()
 			assert.Equal(t, test.expectedCode, w.Code)
-			if test.expectedCode == http.StatusCreated {
+			if test.expectedCode == http.StatusOK {
+				var user models.User
+				err := db.Model(&models.User{}).First(&user, test.Name).Error
+				assert.Nil(t, err)
+				assert.Equal(t, test.Name.Name, user.Name)
+			}
+		})
+	}
+}
+
+var bio string = "Profile 1 bio"
+var profile1 models.User = models.User{
+	ID:       uuid.New(),
+	Name:     "Profile1",
+	BIO:      &bio,
+	Telegram: "@profile1",
+}
+
+type resGetProfile struct {
+	Name      string  `json:"name"`
+	AvatarUrl *string `json:"avatar_url,omitempty"`
+	BIO       *string `json:"bio,omitempty"`
+}
+
+func TestGetProfile(t *testing.T) {
+	fn, quit, err := setUp()
+	assert.Nil(t, err)
+	defer func() {
+		quit <- syscall.SIGTERM
+		fn()
+	}()
+	type Test struct {
+		Expected     models.User
+		jwt          string
+		name         string
+		expectedCode int
+	}
+	tests := []Test{
+		{
+			Expected:     profile1,
+			jwt:          profile1JWT,
+			name:         "get profile 1",
+			expectedCode: http.StatusOK,
+		},
+		{
+			jwt:          unknownJWT,
+			name:         "get unknown profile",
+			expectedCode: http.StatusNotFound,
+		},
+	}
+	db.Create(&profile1)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			url := "/api/user/profile"
+			req, _ := http.NewRequest(http.MethodGet, url, nil) // bytes.NewBuffer(jsonData)
+			req.Header.Set("Authorization", "Bearer "+test.jwt)
+
+			w := httptest.NewRecorder()
+			http3.ServeHTTP(w, req)
+			defer func() {
+				err := w.Result().Body.Close()
+				assert.Nil(t, err)
+			}()
+			assert.Equal(t, test.expectedCode, w.Code)
+			if test.expectedCode == http.StatusOK {
+				var user resGetProfile
+				err := json.Unmarshal(w.Body.Bytes(), &user)
+				assert.Nil(t, err)
+				assert.Equal(t, profile1.Name, user.Name)
+				assert.Equal(t, *profile1.BIO, *user.BIO)
 			}
 		})
 	}
---
+++ internal/tests/migrations/001_init.sql
@@ -0,0 +1,47 @@
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+CREATE TABLE users (
+                       id UUID PRIMARY KEY,
+                       avatar_url VARCHAR DEFAULT NULL,
+                       name VARCHAR NOT NULL UNIQUE,
+                       bio TEXT DEFAULT NULL,
+                       telegram VARCHAR NOT NULL
+);
+
+CREATE TABLE groups (
+                        id UUID PRIMARY KEY,
+                        avatar_url VARCHAR DEFAULT NULL,
+                        name VARCHAR NOT NULL
+);
+
+CREATE TABLE roles (
+                       group_id UUID REFERENCES groups(id),
+                       user_id UUID REFERENCES users(id),
+                       role VARCHAR NOT NULL,
+                       PRIMARY KEY (group_id, user_id)
+);
+
+CREATE TABLE help_requests (
+                               id UUID,
+                               user_id UUID  REFERENCES users(id),
+                               mentor_id UUID REFERENCES users(id),
+                               group_id UUID REFERENCES groups(id),
+                               goal VARCHAR NOT NULL,
+                               bio TEXT DEFAULT NULL,
+                               status VARCHAR NOT NULL,
+                               PRIMARY KEY (id,user_id,mentor_id)
+);
+
+CREATE TABLE pairs (
+                       user_id UUID REFERENCES users(id),
+                       mentor_id UUID REFERENCES users(id),
+                       group_id UUID REFERENCES groups(id),
+                       goal VARCHAR NOT NULL,
+                       PRIMARY KEY (user_id, mentor_id)
+);
+
+-- +goose Down
+-- SQL in section 'Down' is executed when this migration is rolled back
+
+-- Add DROP statements for the UP migration here
