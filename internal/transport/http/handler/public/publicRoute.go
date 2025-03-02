5def24e65f01c450a893362a51cc21b8575eed51
mimimix <lmxsp@mail.ru>
2025-03-02 14:34:30 +0300

    fix online swagger

--- internal/transport/http/handler/public/publicRoute.go
+++ internal/transport/http/handler/public/publicRoute.go
@@ -1,6 +1,9 @@
 package publicRoute
 
 import (
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "gitlab.prodcontest.ru/team-14/lotti/docs"
 	"gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/handler/ApiRouters"
 	"gorm.io/gorm"
 )
@@ -18,5 +21,6 @@ func PublicRoute(apiRouters *ApiRouters.ApiRouters, db *gorm.DB) *Route {
 
 	apiRouters.Public.GET("/ping", router.ping)
 	apiRouters.Public.GET("/mock", router.mocks)
+	apiRouters.Public.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return router
 }
