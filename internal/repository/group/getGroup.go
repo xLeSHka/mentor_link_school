fefb3fdb5130c4958592e6d5ae97837bf1996c57
ainur <ainur.salahiev21@mail.ru>
2025-03-02 15:27:38 +0300

    fix get group

--- internal/repository/group/getGroup.go
+++ internal/repository/group/getGroup.go
@@ -7,8 +7,18 @@ import (
 	"gitlab.prodcontest.ru/team-14/lotti/internal/models"
 )
 
-func (r *GroupRepository) GetGroup(ctx context.Context, userID uuid.UUID, groupID uuid.UUID) (*models.Group, error) {
-	var resp models.Group
-	err := r.DB.Model(&models.Role{}).WithContext(ctx).Where("user_id = ? AND role = 'owner' AND group_id = ?", userID, groupID).First(&resp).Error
-	return &resp, err
+func (r *GroupRepository) GetGroup(ctx context.Context, userID, groupID uuid.UUID) (*models.Group, error) {
+	var group models.Group
+	err := r.DB.WithContext(ctx).
+		Table("groups").
+		Joins("JOIN roles ON roles.group_id = groups.id").
+		Where("roles.user_id = ? AND roles.role = 'owner' AND groups.id = ?", userID, groupID).
+		Select("groups.*").
+		First(&group).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &group, nil
 }
