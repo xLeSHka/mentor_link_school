a1f1d6aa2bda0db98365c1247455e0087a1fa281
ainur <ainur.salahiev21@mail.ru>
2025-03-02 22:56:20 +0300

    profile other

--- internal/transport/http/handler/user/profile.go
+++ internal/transport/http/handler/user/profile.go
@@ -7,6 +7,7 @@ import (
 	"gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/pkg/jwt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 // @Summary Получить инфу о себе
@@ -31,6 +32,48 @@ func (h *Route) profile(c *gin.Context) {
 		return
 	}
 
+	if user.AvatarURL != nil {
+		avatarURL, err := h.minioRepository.GetImage(*user.AvatarURL)
+		if err != nil {
+			err.(*httpError.HTTPError).SendError(c)
+			c.Abort()
+			return
+		}
+		user.AvatarURL = &avatarURL
+	}
+	c.JSON(http.StatusOK, resGetProfile{
+		Name:      user.Name,
+		AvatarUrl: user.AvatarURL,
+		BIO:       user.BIO,
+		Telegram:  user.Telegram,
+	})
+}
+
+func (h *Route) profileOther(c *gin.Context) {
+	_, err := jwt.Parse(c)
+	if err != nil {
+		httpError.New(http.StatusUnauthorized, "Bad id").SendError(c)
+		c.Abort()
+		return
+	}
+
+	id := c.Param("id")
+	if id == "" {
+		httpError.New(http.StatusBadRequest, "Bad id").SendError(c)
+		c.Abort()
+		return
+	}
+	uuid, err := uuid.Parse(id)
+	if err != nil {
+		httpError.New(http.StatusBadRequest, "Bad id").SendError(c)
+		c.Abort()
+		return
+	}
+	user, err := h.usersService.GetByID(c.Request.Context(), uuid)
+	if err != nil {
+		err.(*httpError.HTTPError).SendError(c)
+		return
+	}
 	if user.AvatarURL != nil {
 		avatarURL, err := h.minioRepository.GetImage(*user.AvatarURL)
 		if err != nil {
