c264889bcbfc19848a243ea222fb151899b49e3f
xLeSHka <148469104+xLeSHka@users.noreply.github.com>
2025-03-03 15:44:51 +0300

    edit readme

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,7 +3,6 @@ package app
 import (
 	"gitlab.prodcontest.ru/team-14/lotti/internal/connetions/db"
 	"gitlab.prodcontest.ru/team-14/lotti/internal/connetions/minio"
-	"gitlab.prodcontest.ru/team-14/lotti/internal/integrations"
 	"gitlab.prodcontest.ru/team-14/lotti/internal/transport/http"
 	httpHandlers "gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/handler"
 	"gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/pkg/jwt"
@@ -19,7 +18,6 @@ var App = fx.Options(
 		//redis.New,
 		minio.New,
 	),
-	integrations.Integrations,
 	Repositories,
 	Services,
 	httpHandlers.HttpHandlers,
