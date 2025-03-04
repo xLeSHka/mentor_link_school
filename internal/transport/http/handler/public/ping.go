e231c684ec756528df7f7770e7b14ae1f8b22c06
mimimix <lmxsp@mail.ru>
2025-03-04 03:50:25 +0300

    add debug

--- internal/transport/http/handler/public/ping.go
+++ internal/transport/http/handler/public/ping.go
@@ -2,9 +2,17 @@ package publicRoute
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gitlab.prodcontest.ru/team-14/lotti/internal/transport/http/handler/ws"
 )
 
 func (r *Route) ping(c *gin.Context) {
+	go ws.WriteMessage(&ws.Message{
+		Type:    "request",
+		UserID:  uuid.New(),
+		Request: &ws.Request{},
+	})
+
 	c.JSON(200, gin.H{
 		"message": "PROOOOOOOOOOOOOOOOOD",
 	})
