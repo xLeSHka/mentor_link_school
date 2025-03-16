112ead650742a8f7e88a82195d086562721df012
xLeSHka <148469104+xLeSHka@users.noreply.github.com>
2025-03-17 02:06:02 +0400

    fix create group

--- internal/transport/http/handler/student/students.go
+++ internal/transport/http/handler/student/students.go
@@ -47,10 +47,9 @@ func StudentsRoute(opts FxOpts) *Route {
 	}
 
 	opts.ApiRouter.StudentRoute.GET("/availableMentors", router.availableMentors)
-	opts.ApiRouter.StudentRoute.POST("/requests", router.createRequest)
+	opts.ApiRouter.StudentRoute.POST("/:userID/requests", router.createRequest)
 	opts.ApiRouter.StudentRoute.GET("/requests", router.getRequests)
 	opts.ApiRouter.StudentRoute.GET("/mentors", router.getMyMentors)
-	opts.ApiRouter.StudentRoute.GET("/:userID/requests", router.getMyMentors)
 	//opts.ApiRouter.StudentRoute.POST("/user/invite", router.acceptedInvite)
 
 	return router
