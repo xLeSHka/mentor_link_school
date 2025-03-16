bac0da050cd10e0aadd8d240d72c25f9a0ebc283
xLeSHka <148469104+xLeSHka@users.noreply.github.com>
2025-03-17 01:24:29 +0400

    fix create group

--- internal/repository/group/getMembers.go
+++ internal/repository/group/getMembers.go
@@ -9,7 +9,7 @@ import (
 func (r *GroupRepository) GetMembers(ctx context.Context, groupID uuid.UUID) ([]*models.User, error) {
 	var members []*models.User
 	err := r.DB.Model(&models.User{}).Where("EXISTS (SELECT 1 FROM roles WHERE group_id = ? AND user_id = users.id)", groupID).
-		Preload("Roles").
+		Preload("Roles", "WHERE roles.group_id = ? ", groupID).
 		Find(&members).Error
 	if err != nil {
 		return nil, err
